9e47c0870d8d0cf813301e03fd32a2466402bbbc
lerrylerry <laungayanjohnlerry@gmail.com>
2022-12-14 16:19:43 +0800

    fixer

--- component/Food.jsx
+++ component/Food.jsx
@@ -6,34 +6,34 @@ const Food = () => {
         <div className={styles.container}>
             <div className={styles.row}>
                 <div className={styles.col_1}>
-                    <Link className={styles.col_2} href="http://localhost:3000/bulalo">
+                    <Link className={styles.col_2} href="/bulalo">
                     <img src="/images/bulalo.png"/>
                     <p>BULALO</p>
                     </Link>
 
-                    <Link className={styles.col_2} href="http://localhost:3000/bukopie">
+                    <Link className={styles.col_2} href="/bukopie">
                     <img src="/images/buko-pie.png"/>
                     <p>BUKO PIE</p>
                     </Link>
                 </div>
                 <div className={styles.col_2}>
-                    <Link className={styles.col_2} href="http://localhost:3000/bukotart"> 
+                    <Link className={styles.col_2} href="/bukotart"> 
                     <img src="/images/buko-tarts.png"/>
                     <p>BUKO TARTS</p>
                     </Link>
 
-                    <Link className={styles.col_2} href="http://localhost:3000/satay">
+                    <Link className={styles.col_2} href="/satay">
                     <img src="/images/chicken-satay.png"/>
                     <p>CHICKEN SATAY</p>
                     </Link>
                 </div>
                 <div className={styles.col_3}>
-                    <Link className={styles.col_2} href="http://localhost:3000/champorado">
+                    <Link className={styles.col_2} href="/champorado">
                     <img src="/images/champorado.png"/>
                     <p>CHAMPORADO</p>
                     </Link>
 
-                    <Link className={styles.col_2} href="http://localhost:3000/piaya">
+                    <Link className={styles.col_2} href="/piaya">
                     <img src="/images/piaya.png"/>
                     <p>BALAY DAKO’S PIAYA &<br/>NAPOLEONES</p>
                     </Link>
--- component/TouristSpot.jsx
+++ component/TouristSpot.jsx
@@ -6,34 +6,34 @@ const TouristSpot = () => {
         <div className={styles.container}>
             <div className={styles.row}>
                 <div className={styles.col_1}>
-                    <Link className={styles.col_2} href="http://localhost:3000/skyranch">
+                    <Link className={styles.col_2} href="/skyranch">
                     <img src="/images/sky-ranch.png"/>
                     <p>SKYRANCH TAGAYTAY</p>
                     </Link>
 
-                    <Link className={styles.col_2} href="http://localhost:3000/ourladylourdes">
+                    <Link className={styles.col_2} href="/ourladylourdes">
                     <img src="/images/our-lady.png"/>
                     <p>OUR LADY OF LOURDES<br/>PARISH CHURCH</p>
                     </Link>
                 </div>
                 <div className={styles.col_2}>
-                    <Link className={styles.col_2} href="http://localhost:3000/taalvolc">
+                    <Link className={styles.col_2} href="/taalvolc">
                     <img src="/images/taal-aerial.png"/>
                     <p>TAAL VOLCANO</p>
                     </Link>
                     
-                    <Link className={styles.col_2} href="http://localhost:3000/picnicgroove">
+                    <Link className={styles.col_2} href="/picnicgroove">
                     <img src="/images/picnic-groove.png"/>
                     <p>PICNIC GROOVE</p>
                     </Link>
                 </div>
                 <div className={styles.col_3}>
-                    <Link className={styles.col_2} href="http://localhost:3000/puzzlemansion">
+                    <Link className={styles.col_2} href="/puzzlemansion">
                     <img src="/images/picnic-groove.png"/>
                     <p>PUZZLE MANSION</p>
                     </Link>
 
-                    <Link className={styles.col_2} href="http://localhost:3000/peoplespark">
+                    <Link className={styles.col_2} href="/peoplespark">
                     <img src="/images/peoples-park.png"/>
                     <p>PEOPLES PARK IN<br/>THE SKY</p>
                     </Link>
